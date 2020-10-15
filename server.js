6817c68b4e32c924efa70b1967929b8c145780bd
Mera Youssef <merayoussef91@hotmail.com>
2020-10-14 19:01:46 -0500

    connected html pages from public file

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+// middleware
 // parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// connected the style and javascript code in /public folder.
+app.use(express.static('public'));
 
 const { animals } = require('./data/animals');
-
-
-
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -100,6 +101,19 @@ app.get('/api/animals/:id', (req, res) => {
     } else {
         res.send(404);
     }
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+// calling the html file to render on file call.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/animals', (req, res) => {
